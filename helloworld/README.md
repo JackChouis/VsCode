f27cbb9f15a86c9f2ac0f8749701681c1a5a9b91
JackChouis <35693199+JackChouis@users.noreply.github.com>
2019-10-04 23:37:41 +0800

    test

---
+++ helloworld/README.md
@@ -0,0 +1,12 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-04 22:33:45
+ * @LastEditTime: 2019-10-04 22:33:45
+ * @LastEditors: your name
+ -->
+# vscode
+VScodeCode
+啦啦啦啦
+test
+test
---
+++ helloworld/helloworld.c
@@ -0,0 +1,35 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-04 19:23:10
+ * @LastEditTime: 2019-10-04 22:26:48
+ * @LastEditors: Please set LastEditors
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * @description: 
+ * @param {type} 
+ * @return: 
+ */
+int main()
+{
+    printf("hello world\n");
+
+    for(int i=0;i<8;i++)
+    {
+        printf("%d\n",i);
+        printf("what the fuck!");
+        printf("what waht a fuck");
+        printf("5");
+        printf("6");
+        printf("7");
+        printf("8");
+        printf("9");
+
+    }
+
+    system("pause");
+    return 0;
+}
