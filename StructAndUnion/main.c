cb6d8b16b03c4745e8a9cf000ad9870a2afda9f0
JackChouis <zyzy7758521@foxmail.com>
2022-03-17 01:18:33 +0800

    tt

---
+++ StructAndUnion/main.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: JackChouis
+ * @Date: 2022-03-17 01:08:39
+ * @LastEditors: JackChouis
+ * @LastEditTime: 2022-03-17 01:14:01
+ * @FilePath: \VsCode\StructAndUnion\main.c
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by JackChouis, All Rights Reserved. 
+ */
+#include<stdio.h>
+
+void main()
+{
+    printf("test");
+    
+}
