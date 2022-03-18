64c93d7a0ed6b62d2678f948cb6afb9fc37b807c
JackChouis <zyzy7758521@foxmail.com>
2022-03-18 21:16:53 +0800

    多C文件

--- StructAndUnion/main.c
+++ StructAndUnion/main.c
@@ -2,16 +2,35 @@
  * @Author: JackChouis
  * @Date: 2022-03-17 01:08:39
  * @LastEditors: JackChouis
- * @LastEditTime: 2022-03-17 01:14:01
+ * @LastEditTime: 2022-03-18 21:13:50
  * @FilePath: \VsCode\StructAndUnion\main.c
  * @Description: 
  * 
  * Copyright (c) 2022 by JackChouis, All Rights Reserved. 
  */
 #include<stdio.h>
+#include "test.h"
+extern int add();
+int t = 0;
+typedef struct 
+{
+    unsigned int Year;
+    unsigned int month;
+    unsigned int day;
+    unsigned int hour;
+    unsigned int minutes;
+    unsigned int second;
+}TypeTime;
 
+union Data{
+    int i;
+    float f;
+    char str[7];
+    double d;
+}data;
 void main()
 {
-    printf("test");
-    
+    int s = add();
+    printf("s value is %d\n", s);
+    //   printf("test%d",sizeof(data));
 }
---
+++ StructAndUnion/test.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: JackChouis
+ * @Date: 2022-03-18 20:46:50
+ * @LastEditors: JackChouis
+ * @LastEditTime: 2022-03-18 21:16:37
+ * @FilePath: \VsCode\StructAndUnion\test.c
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by JackChouis, All Rights Reserved. 
+ */
+#include "test.h"
+
+int add()
+{
+    t = t + 4;
+    return t;
+}
